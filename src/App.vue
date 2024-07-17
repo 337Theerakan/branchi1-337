6fcc94cde62202d312a1ae9a2e9bcdf9324778a5
337Theerakan <theerakankhankham.tk@gmail.com>
2024-07-17 11:28:39 +0700

    modified template in app.vue

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,9 @@
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div> <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
